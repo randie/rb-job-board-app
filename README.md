a699398d9584e9e1c996bba125d9a0061fd18c28
Randie B <randie.bemis@gmail.com>
2020-01-12 23:11:36 -0800

    initial app without graphql

---
+++ README.md
@@ -0,0 +1,3 @@
+# Job Board
+
+> **Note:** WIP. This is a learn-by-doing toy web app to play with GraphQL
---
+++ client/src/app.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { isLoggedIn, logout } from './auth';
+import { CompanyDetail } from './company-detail';
+import { LoginForm } from './login-form';
+import { JobBoard } from './job-board';
+import { JobDetail } from './job-detail';
+import { JobForm } from './job-form';
+import { NavBar } from './nav-bar';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loggedIn: isLoggedIn() };
+  }
+
+  handleLogin() {
+    this.setState({ loggedIn: true });
+    this.router.history.push('/');
+  }
+
+  handleLogout() {
+    logout();
+    this.setState({ loggedIn: false });
+    this.router.history.push('/');
+  }
+
+  render() {
+    const { loggedIn } = this.state;
+    return (
+      <Router ref={router => (this.router = router)}>
+        <div>
+          <NavBar loggedIn={loggedIn} onLogout={this.handleLogout.bind(this)} />
+          <section className="section">
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={JobBoard} />
+                <Route path="/companies/:companyId" component={CompanyDetail} />
+                <Route exact path="/jobs/new" component={JobForm} />
+                <Route path="/jobs/:jobId" component={JobDetail} />
+                <Route
+                  exact
+                  path="/login"
+                  render={() => <LoginForm onLogin={this.handleLogin.bind(this)} />}
+                />
+              </Switch>
+            </div>
+          </section>
+        </div>
+      </Router>
+    );
+  }
+}
