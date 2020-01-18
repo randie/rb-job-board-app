db65a5b52fcb0a76fe6d6899539e69a373b07946
Randie B <randie.bemis@gmail.com>
2020-01-17 17:31:35 -0800

    add fetchJob() to requests, update job-details to fetch a job by id using graphql job query

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,16 +1,18 @@
 const endpointUrl = 'http://localhost:9000/graphql';
 
 export async function fetchJobs() {
-  const query = `{
-    jobs {
-      id
-      title
-      company {
+  const query = `
+    {
+      jobs {
         id
-        name
+        title
+        company {
+          id
+          name
+        }
       }
     }
-  }`;
+  `;
   const options = {
     method: 'POST',
     headers: { 'content-type': 'application/json' },
@@ -19,4 +21,28 @@ export async function fetchJobs() {
   const response = await fetch(endpointUrl, options);
   const responseBody = await response.json();
   return responseBody.data.jobs;
+}
+
+export async function fetchJob(id) {
+  const query = `
+    query JobQuery($id: ID!) {
+      job(id: $id) {
+        id
+        title
+        company {
+          id
+          name
+        }
+        description
+      }
+    }
+  `;
+  const options = {
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify({ query, variables: { id } }),
+  };
+  const response = await fetch(endpointUrl, options);
+  const responseBody = await response.json();
+  return responseBody.data.job;
 }
